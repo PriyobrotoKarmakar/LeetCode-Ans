3d917c30541f0f2ccd7bad41518b7a312e8018d4
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-02 15:12:12 +0530

    Time: 119 ms (11.09%), Space: 42.9 MB (72.64%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp;
+        temp = arr;
+        sort(temp.begin(), temp.end());
+        map<int, int> rank;
+        int R = 1;
+        for (auto ele : temp) {
+            if (rank[ele] == 0) {
+                rank[ele] = R++;
+            }
+        }
+        temp.clear();
+        for (auto ele : arr) {
+            temp.push_back(rank[ele]);
+        }
+        return temp;
+    }
+};
