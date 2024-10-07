56927364f0ec739a29e70d2012c367de9b945af1
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-07 12:53:07 +0530

    Update 2800-minimum-string-length-after-removing-substrings.cpp

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
-    int minLength(string s) {
+//     int minLength(string s) {
+//         stack<char> st;
+//         for(int i=0;i<s.length();i++){
+//         if (!st.empty() and ((st.top() == 'A' and s[i] == 'B') or
+//                        (st.top() == 'C' and s[i] == 'D'))) {
+//                 st.pop();
+//             } else {
+//                 st.push(s[i]);
+//             }
+//     }   
+// return st.size();
+//     }
+int minLength(string s){
         int i = 0;
         int j = 1;
         while (j < s.length()) {
