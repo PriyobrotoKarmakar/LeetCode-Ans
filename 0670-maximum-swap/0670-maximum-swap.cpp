7ef5422dad68aebf890dfa7ff880dec1d81f41b7
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-17 23:43:19 +0530

    Time: 0 ms (100%), Space: 7.6 MB (70.34%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    priority_queue<int,vector<int>>pq;
-    
+    // priority_queue<int,vector<int>>pq;
+
     int maximumSwap(int num) {
-        pq.push(num);
+        // pq.push(num);
         string N = to_string(num);
-        
+
+        // for(int i=0;i<N.length();i++){
+        //     for(int j=i+1;j<N.length();j++){
+        //         swap(N[i],N[j]);
+        //         pq.push(stoi(N));
+        //         swap(N[i],N[j]);
+        //     }
+        // }
+        // return pq.top();
+        vector<int> maxRight(10, -1);
+        for (int i = 0; i < N.length(); i++) {
+            int currDigit = N[i]-'0';
+            maxRight[currDigit] = i;
+        }
+
         for(int i=0;i<N.length();i++){
-            for(int j=i+1;j<N.length();j++){
-                swap(N[i],N[j]);
-                pq.push(stoi(N));
-                swap(N[i],N[j]);
+            int currDigit = N[i]-'0';
+            for(int j=9;j>currDigit;j--){
+                int maxInd = maxRight[j];
+                if(maxInd!=-1 && maxInd>i){
+                    swap(N[i],N[maxInd]);
+                    return stoi(N);
+                }
             }
         }
-        return pq.top();
+        return num;
     }
 };
