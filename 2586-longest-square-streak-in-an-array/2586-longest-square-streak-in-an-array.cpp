86707d8fc30f309568f0e8f68e6fc89d722b717c
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-10-29 00:00:03 +0530

    Time: 169 ms (13.33%), Space: 111.4 MB (37.62%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,21 +1,38 @@
+// class Solution {
+// public:
+//     int longestSquareStreak(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         unordered_map<float,int>streak;
+//         for(int i=0;i<nums.size();i++){
+//             if(streak[sqrt(nums[i])]!=0){
+//                 streak[nums[i]] = streak[sqrt(nums[i])]+1;
+//             }else{
+//                 streak[nums[i]]=1;
+//             }
+//         }
+//         int maxi  = INT_MIN;
+//         for(const pair<float,int>& pair:streak){
+//             maxi = max(maxi,pair.second);
+//         }
+//         if(maxi==1)return -1;
+
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        unordered_map<float,int>streak;
-        for(int i=0;i<nums.size();i++){
-            if(streak[sqrt(nums[i])]!=0){
-                streak[nums[i]] = streak[sqrt(nums[i])]+1;
-            }else{
-                streak[nums[i]]=1;
-            }
+        map<int, int>mp;
+        sort(nums.begin(), nums.end());
+        int res = -1;
+        for(int num: nums) {
+            int _sqrt = sqrt(num);
+            if(_sqrt*_sqrt == num && mp.find(_sqrt)!=mp.end()) {
+                mp[num] = mp[_sqrt]+1;
+                res = max(res, mp[num]);
+            } else mp[num] = 1;
         }
-        int maxi  = INT_MIN;
-        for(const pair<float,int>& pair:streak){
-            maxi = max(maxi,pair.second);
-        }
-        if(maxi==1)return -1;
-
-        return maxi;
+        return res;
     }
 };
