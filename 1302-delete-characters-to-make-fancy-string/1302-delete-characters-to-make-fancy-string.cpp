1a2c1ffc1796446f846f868bcc225f6ac2cda85b
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-11-01 19:46:49 +0530

    Time: 49 ms (5.19%), Space: 42.8 MB (30.23%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     string makeFancyString(string s) {
+        // string ans = "";
+        // for (int i=0;i<s.length();i++){
+        //     char currChar = s[i];
+        //     if(ans.length()!=0 and ans[ans.size()-1]==currChar){
+        //         char temp = ans[ans.size()-1];
+        //         ans.pop_back();
+        //         if(ans.length()!=0 and ans[ans.size()-1]==currChar){
+        //             ans.pop_back();
+        //         }
+        //         ans.push_back(temp);
+        //     }
+        //     ans.push_back(currChar);
+        // }
+        // return ans;
+        int i=0;
         string ans = "";
-        for (int i=0;i<s.length();i++){
-            char currChar = s[i];
-            if(ans.length()!=0 and ans[ans.size()-1]==currChar){
-                char temp = ans[ans.size()-1];
-                ans.pop_back();
-                if(ans.length()!=0 and ans[ans.size()-1]==currChar){
-                    ans.pop_back();
-                }
-                ans.push_back(temp);
+        while(i<s.length()){
+            int j = i-1;
+            int k = i-2;
+            if(j>=0 and k>=0 and s[j]==s[i] and s[k]==s[i]){
+                
+            }else{
+                ans+=s[i];
             }
-            ans.push_back(currChar);
+            i++;
         }
         return ans;
     }
