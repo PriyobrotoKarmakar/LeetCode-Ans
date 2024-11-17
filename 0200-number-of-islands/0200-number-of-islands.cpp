832da90f85e1dbedceaac7d22a1a48a38d27cb0b
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-11-17 21:06:18 +0530

    Time: 37 ms (22.6%), Space: 23.8 MB (23.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,51 +1,51 @@
 class Solution {
-private:
-    void BFS(int i, int j, vector<vector<char>>& grid,
-             vector<vector<bool>>& visited) {
+public:
+    void BFS(int i,int j,vector<vector<char>>& grid,vector<vector<bool>>&visited){
         int m = grid.size();
         int n = grid[0].size();
-
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        visited[i][j] = true;
-
-        
-        vector<pair<int, int>> direction{
-            {-1, 0}, {1, 0}, {0, -1}, {0, 1}, // Up, Down, Left, Right
+        vector<pair<int,int>>dir{
+            {0,-1},//left
+            {0,1},//right
+            {1,0},//up
+            {-1,0}//down
         };
 
-        while (!q.empty()) {
-            auto [x, y] = q.front();
+        visited[i][j] = true;
+
+        queue<pair<int,int>>q;
+        q.push({i,j});
+
+        while(!q.empty()){
+            auto [x,y] = q.front();
             q.pop();
-            for (auto [dx, dy] : direction) {
-                int nx = x + dx, ny = y + dy;
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n && 
-                    !visited[nx][ny] && grid[nx][ny] == '1') {
-                    q.push({nx, ny});
+
+            for(auto [dx,dy] : dir){
+                int nx = dx+x, ny = dy+y;
+                if(nx>= 0 && nx<m && ny>=0 && ny<n && !visited[nx][ny] && grid[nx][ny]=='1'){
+                    q.push({nx,ny});
                     visited[nx][ny] = true;
                 }
             }
-        }
-    }
 
-public:
+        }
+
+    }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
         int island = 0;
 
-       
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-            
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    BFS(i, j, grid, visited);
+        for(int i=0;i<m;i++){
+            for(int j = 0; j < n ; j ++){
+                if(!visited[i][j] && grid[i][j]=='1'){
+                    BFS(i,j,grid,visited);
                     island++;
                 }
             }
         }
+
         return island;
     }
 };
