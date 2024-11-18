b431520c57205421b717b0fa8130ebd48ca9870d
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-11-18 15:47:27 +0530

    Time: 0 ms (100%), Space: 10.4 MB (17.93%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -33,16 +33,31 @@ public:
         vector<int>ans;
         ans = arr;
         int n = arr.size();
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=1;j<=k;j++){
-                if((i-j)<0){
-                    sum+=arr[n+(i-j)];
-                }else{
-                    sum+=arr[i-j];
-                }
-            }
-            ans[i] = sum;
+        // for(int i=0;i<n;i++){
+        //     int sum = 0;
+        //     for(int j=1;j<=k;j++){
+        //         if((i-j)<0){
+        //             sum+=arr[n+(i-j)];
+        //         }else{
+        //             sum+=arr[i-j];
+        //         }
+        //     }
+        //     ans[i] = sum;
+        // }
+        int i = n-2;
+        int j = n-2;
+        int sum = 0;
+        for(int m = 0;m<k;m++){
+            sum+=arr[j];
+            j = (j-1)<0 ? n+(j-1) : j-1; 
+        }
+        ans[n-1] = sum;
+        for(int m = n-2;m>=0;m--){
+            sum-=arr[i];
+            i = (i-1)<0 ? n+(i-1) : i-1;
+            sum+=arr[j];
+            j = (j-1)<0 ? n+(j-1) : j-1; 
+            ans[m] = sum;
         }
         arr = ans;
     }
