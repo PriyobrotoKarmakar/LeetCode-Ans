0ff36f6c0acc3d1297890b5499753db8c54e06c4
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-11-18 16:06:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (84%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    void sumOfNext(vector<int>& arr, int k){
-        vector<int>ans;
-        ans = arr;
+    void sumOfNext(vector<int>& arr, int k) {
         int n = arr.size();
-        int i=1;
-        int j=1;
+        vector<int> result(n);
         int sum = 0;
-        for(int m=0;m<k;m++){
-            sum+=arr[j];
-            j = (j+1)%n;
+        // first window
+        for (int m = 0, j = 1; m < k; m++, j = (j + 1) % n) {
+            sum += arr[j];
         }
-        ans[0] = sum; 
-        for(int m=1;m<n;m++){
-            sum-=arr[i];
-            i = (i+1)%n;
-            sum+=arr[j];
-            j = (j+1)%n;
-            ans[m] = sum;
-        }
-        // for(int i=0;i<n;i++){
-        //     int sum = 0;
-        //     for(int j=1;j<=k;j++){
-        //         sum+=arr[(i+j)%n];
-        //     }
-        //     ans[i] = sum;
-        // }
+        result[0] = sum;
 
-        arr = ans;
+        for (int m = 1, i = 1, j = (k + 1) % n; m < n; m++,
+             j = (j + 1) % n, i = (i + 1) % n) {
+        
+                sum-=arr[i];
+                sum+=arr[j];
+
+                result[m] = sum;
+        }
+        arr = result;
     }
-    void sumOfPrev(vector<int>& arr, int k){
-        vector<int>ans;
+    void sumOfPrev(vector<int>& arr, int k) {
+        vector<int> ans;
         ans = arr;
         int n = arr.size();
         // for(int i=0;i<n;i++){
@@ -44,32 +35,32 @@ public:
         //     }
         //     ans[i] = sum;
         // }
-        int i = n-2;
-        int j = n-2;
+        int i = n - 2;
+        int j = n - 2;
         int sum = 0;
-        for(int m = 0;m<k;m++){
-            sum+=arr[j];
-            j = (j-1)<0 ? n+(j-1) : j-1; 
+        for (int m = 0; m < k; m++) {
+            sum += arr[j];
+            j = (j - 1) < 0 ? n + (j - 1) : j - 1;
         }
-        ans[n-1] = sum;
-        for(int m = n-2;m>=0;m--){
-            sum-=arr[i];
-            i = (i-1)<0 ? n+(i-1) : i-1;
-            sum+=arr[j];
-            j = (j-1)<0 ? n+(j-1) : j-1; 
+        ans[n - 1] = sum;
+        for (int m = n - 2; m >= 0; m--) {
+            sum -= arr[i];
+            i = (i - 1) < 0 ? n + (i - 1) : i - 1;
+            sum += arr[j];
+            j = (j - 1) < 0 ? n + (j - 1) : j - 1;
             ans[m] = sum;
         }
         arr = ans;
     }
     vector<int> decrypt(vector<int>& code, int k) {
-        if(k==0){
-            for(int i=0;i<code.size();i++){
+        if (k == 0) {
+            for (int i = 0; i < code.size(); i++) {
                 code[i] = 0;
             }
-        }else if(k>0){
-            sumOfNext(code,k);
-        }else{
-            sumOfPrev(code,(-1)*k);
+        } else if (k > 0) {
+            sumOfNext(code, k);
+        } else {
+            sumOfPrev(code, (-1) * k);
         }
         return code;
     }
