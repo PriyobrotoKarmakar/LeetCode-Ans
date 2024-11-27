93c3f710bb2da1c13b465a8952fc7bb878122e2b
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2024-11-27 19:25:01 +0530

    Time: 8 ms (99.05%), Space: 52.1 MB (97.4%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,51 +1,30 @@
-
 class Solution {
-    void createAdjList(vector<vector<int>>& adj, int n) {
-        for (int i = 0; i < n - 1; i++) {
-            adj[i].push_back(i + 1);
+    void dfs(vector<vector<int>>& tree, int c, vector<int>& dp) {
+        int d=dp[c]+1;
+        for (int x : tree[c]) {
+            if (dp[x]<=d) continue;
+            dp[x]=d;
+            dfs(tree,x,dp);
         }
     }
-
-    void DFS(int src, vector<int>& topo, vector<int>& vis, const vector<vector<int>>& adj) {
-        vis[src] = 1;
-        for (int node : adj[src]) { 
-            if (!vis[node]) DFS(node, topo, vis, adj);
-        }
-        topo.push_back(src); 
-    }
-
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n); 
-        createAdjList(adj, n); 
-        vector<int> ans;
-        for (auto& edge : queries) { 
-            int u = edge[0];
-            int v = edge[1];
-            adj[u].push_back(v); 
-
-            vector<int> topo;
-            vector<int> vis(n, 0);
-            for (int i = 0; i < n; ++i) {
-                if (!vis[i]) DFS(i, topo, vis, adj); 
-            }
-            reverse(topo.begin(), topo.end()); 
-
-            vector<int> dist(n, 1e9);
-            dist[0] = 0; 
-
-            for (int node : topo) {
-                if (dist[node] != 1e9) { 
-                    for (int neighbor : adj[node]) { 
-                        if (dist[neighbor] > dist[node] + 1) {
-                            dist[neighbor] = dist[node] + 1;
-                        }
-                    }
-                }
-            }
-
-            ans.push_back(dist[n - 1]); 
+        vector<int> dp(n);
+        for (int i=0; i<n; ++i) dp[i]=n-1-i;
+        vector<vector<int>> tree(n);
+        for (int i=0; i+1<n; ++i)
+            tree[i+1].push_back(i);
+        int m=int(queries.size());
+        vector<int> res(m);
+        int i=0;
+        for (auto& q : queries) {
+            int a=q[0], b=q[1];
+            tree[b].push_back(a);
+            dp[a]=min(dp[a],dp[b]+1);
+            dfs(tree,a,dp);
+            res[i]=dp[0];
+            ++i;
         }
-        return ans; 
+        return res;
     }
 };
