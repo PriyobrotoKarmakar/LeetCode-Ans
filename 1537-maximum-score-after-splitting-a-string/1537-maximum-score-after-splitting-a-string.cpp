021df5e0d392a9c9d1c1b5ffd36106806ca084b6
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-01-01 21:03:24 +0530

    Time: 0 ms (100%), Space: 8.7 MB (38.26%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int numberOfZeros(string s){
-        int count = 0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='0')count++;
-        }
-        return count;
-    }
-    int numberOfOnes(string s){
-        int count = 0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1')count++;
-        }
-        return count;
-    }
-
     int maxScore(string s) {
+        int ones = count(s.begin(),s.end(),'1');
+        int zeros = 0;
         int ans = 0;
-        for(int i=1;i<s.length();i++){
-            int j = s.length()-i;
-            string zero = s.substr(0,i);
-            string one = s.substr(i,j);
-            ans = max(ans,(numberOfZeros(zero)+numberOfOnes(one)));
+        cout<<ones;
+        for(int i=0;i<s.length()-1;i++){
+            if(s[i]=='1'){
+                ones--;
+            }else{
+                zeros++;
+            }
+            ans  = max(ans,(ones+zeros));
         }
         return ans;
     }
