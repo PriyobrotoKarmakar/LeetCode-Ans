e668bc664454d925fe0ac8b842853e98741518f0
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-03-05 17:31:08 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.69%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
+    long long coloredCells(long long n) {
         long long ans = 1;
-        while(n>=1){
-            ans+=(4*(n-1));
-            n--;
-        }
+        // while(n>=1){
+        //     ans+=(4*(n-1));
+        //     n--;
+        // }
+        ans = ans+4*(n*(n-1)/2);
         return ans;
     }
 };
