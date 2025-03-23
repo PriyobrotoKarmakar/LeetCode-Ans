c4e9f59afa2da26c61031103d1db438d7efc151a
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-03-23 15:11:46 +0530

    Time: 3 ms (64.46%), Space: 18.4 MB (80.79%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,46 +8,43 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- class lambda{
-    public:
-        bool operator()(ListNode* &a,ListNode* &b){
-            return a->val > b->val;
-        }
- };
+class comparetor {
+public:
+    bool operator()(ListNode*& a, ListNode*& b) { return a->val > b->val; }
+};
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*>,lambda>pq;
+        priority_queue<ListNode*, vector<ListNode*>, comparetor> pq;
         int row = lists.size();
-        for(int i= 0;i<row;i++){
-            ListNode* temp =lists[i];
-            if(temp!=NULL)
+        for (int i = 0; i < row; i++) {
+            ListNode* temp = lists[i];
+            if (temp != NULL) {
                 pq.push(temp);
+            }
         }
 
         ListNode* head = NULL;
         ListNode* tail = NULL;
 
-
-        while(!pq.empty()){
+        while (!pq.empty()) {
             ListNode* front = pq.top();
             pq.pop();
 
-            //insertAtTail;
-            if(head==NULL && tail==NULL){
+            // insertingAtTail
+            if (head == NULL and tail == NULL) {
                 head = front;
-                tail  = front;
-            }else{
+                tail = front;
+            } else {
                 tail->next = front;
                 tail = front;
             }
 
-            //update the queue
-            if(tail->next!=NULL){
-                pq.push(tail->next);
+            if (front->next != NULL) {
+                pq.push(front->next);
             }
         }
+
         return head;
-        
     }
 };
