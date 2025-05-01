9ff1b5890b65cdf0aa3e3afff7d81103945d6ddc
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-05-02 00:21:25 +0530

    Time: 821 ms (34.11%), Space: 286.4 MB (52.33%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     bool isValid(vector<int>& tasks, vector<int>& workers, int pills, int strength, int& mid){
-        multiset<int> mst(workers.begin(),workers.begin()+mid);
+        multiset<int> mst(workers.begin(),workers.begin()+mid); //mid
+        //mid
         int pillUsed = 0;
-        for(int i = mid-1;i>=0;i--){
+
+        //mid+ (mid*mid)
+        for(int i = mid-1;i>=0;i--){ //mid
             int taskToComplete = tasks[i];
             auto it = prev(mst.end());
             if(*it>=taskToComplete){
-                mst.erase(it);
+                mst.erase(it); //logmid
             }else if(pillUsed>=pills){
                 return false;
             }else{
-                auto weakWorker = mst.lower_bound(taskToComplete-strength);
+                auto weakWorker = mst.lower_bound(taskToComplete-strength); //mid
                 if(weakWorker==mst.end()){
                     return false;
                 }
-                mst.erase(weakWorker);
+                mst.erase(weakWorker); //logmid
                 pillUsed++;
             }
         }
@@ -25,14 +28,14 @@ public:
         int n = tasks.size();
         int m = workers.size();
 
-        sort(tasks.begin(),tasks.end());
-        sort(workers.begin(),workers.end(),greater());
+        sort(tasks.begin(),tasks.end()); //nlogn
+        sort(workers.begin(),workers.end(),greater()); //mlogm
 
-
+         // T.C: nlogn + mlogm + (logn * (logn+ (logn * logn)));
         int start = 0;
         int end = min(m,n);
         int res = 0;
-        while(start<=end){
+        while(start<=end){  //logn
             int mid = start + (end-start)/2;
 
             if(isValid(tasks,workers,pills,strength,mid)){
