6e751af496c6c3408d27a96989aa5cad55699bd7
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-04 09:24:20 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.36%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(auto ele: nums){
+            ans^=ele;
+        }
+        return ans;
+    }
+};
