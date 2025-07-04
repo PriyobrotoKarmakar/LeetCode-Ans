0c582cf6421f18bee55a04b639a71cd1659b556f
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-04 10:07:47 +0530

    Time: 7 ms (65.11%), Space: 26 MB (69.56%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    bool isNotCommon(string& a, string& b){
-        int mask = 0;
-        for(char ch: a){
-            mask =  mask | (1<<(ch-'a'+1));
-        }
-
-        for(char ch: b){
-            if(mask & (1<<(ch-'a'+ 1))){
-                return false;
-            }
-        }
-        return true;
-    }
     int maxProduct(vector<string>& words) {
         int n = words.size();
-        int ans = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(isNotCommon(words[i],words[j])){
+        vector<int> mask(n);
+        for (int i = 0; i < n; i++) {
+            int smallMask = 0;
+            for (char ch : words[i]) {
+                smallMask = smallMask | (1 << (ch - 'a' + 1));
+            }
+            mask[i] = smallMask;
+        }
+        int ans= 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if((mask[i] & mask[j])==0){
                     int lengths = words[i].length() * words[j].length();
                     ans = max(ans,lengths);
                 }
