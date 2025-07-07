b3004a16a04b1becbb54e37c8e43244295457a43
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-07 09:20:16 +0530

    Time: 3 ms (67.06%), Space: 8.4 MB (10.95%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -6,8 +6,18 @@ public:
         dp[n] = DP(n-1,dp)+DP(n-2,dp);
         return dp[n];
     }
+    int DP_Bottom_UP(int n){
+        vector<int>dp(n+2,-1);
+        dp[0] = 0;
+        
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     int fib(int n) {
         vector<int>dp(n+1,-1);
-        return DP(n,dp);
+        return DP_Bottom_UP(n);
     }
 };
