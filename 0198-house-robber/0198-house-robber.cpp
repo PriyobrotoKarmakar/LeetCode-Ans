fb50d76ce0a52227e6774e14d5aa3ff7fefb4024
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-07 10:22:13 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int index,vector<int>&dp){
-        if(index>=nums.size())
-            return 0;
+    int solve( vector<int>& nums,int i,vector<int>&dp){
+        if(i==0)return nums[i];
+        if(i<0)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int inc = nums[i] + solve(nums,i-2,dp);
+        int exc = 0 + solve(nums,i-1,dp);
 
-
-        if(dp[index]!=-1)
-            return dp[index];
-
-        int include = nums[index] + solve(nums,index+2,dp);
-        int exclude = 0 + solve(nums,index+1,dp);
-
-        
-        dp[index] = max(include,exclude);
-        return dp[index];   
-    }
-    int solveTobulation(vector<int>& nums){
-        int n = nums.size();
-        int next1 = 0;
-        int next2 = 0;
-        for(int i=n-1;i>=0;i--){
-            int include = nums[i] + next2;
-            int exclude = 0 + next1;
-            int curr = max(include,exclude);
-
-            next2 = next1;
-            next1 = curr;
-        }
-        return next1;
+        return dp[i] = max(inc,exc);
     }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        int index = 0;
-        // return solve(nums,index,dp);
-        return solveTobulation(nums);
+        int n = nums.size();
+        vector<int>dp(n+1,-1);
+        int i = 0;
+        int money = 0;
+        return solve(nums,n-1,dp);
     }
 };
