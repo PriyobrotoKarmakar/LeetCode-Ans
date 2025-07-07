160c75fc32456c455ec46e24d52d5ed63f877384
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-07 10:58:52 +0530

    Time: 0 ms (100%), Space: 9.5 MB (25.38%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(string& s, int i,vector<int>& dp){
+        if(i==s.length())return 1;
+        if(s[i]=='0')return 0;
+        if(dp[i]!=-1)return dp[i];
+
+        int ways = solve(s,i+1,dp);
+        if(i+1<s.length()){
+            int validNum = stoi(s.substr(i,2));
+            if(validNum>=10 && validNum<=26){
+                ways += solve(s,i+2,dp);
+            }
+        }
+        dp[i] = ways;
+        return dp[i];
+    }
+    int numDecodings(string s) {
+        vector<int>dp(s.length()+1,-1);
+        return solve(s,0,dp);
+    }
+};
