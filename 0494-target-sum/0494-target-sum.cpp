725dfc31b7b30212d0255984596d625d9184c758
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-08 11:48:48 +0530

    Time: 273 ms (29.58%), Space: 78.2 MB (6.71%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,23 @@
+#define ll long long
+class Solution {
+public:
+    ll solve(vector<int>& arr, int target, int idx, ll sum,
+             map<array<ll,2>,ll>& dp) {
+        if (idx >= arr.size())
+            return sum == target ? 1 : 0;
+
+        if (dp.count({idx,sum}))
+            return dp[{idx,sum}];
+        ll subtract = solve(arr, target, idx + 1, sum - arr[idx], dp);
+        ll add = solve(arr, target, idx + 1, sum + arr[idx], dp);
+
+        return dp[{idx,sum}] = subtract + add;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        // vector<vector<int>>dp(n,vector<int>(2*sum + 1,-1));
+        map<array<ll,2>,ll> dp;
+        return solve(nums, target, 0, 0LL, dp);
+    }
+};
