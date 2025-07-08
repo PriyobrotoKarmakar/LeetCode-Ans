51f60485d7f69a737ba40b00aac0ba22272b2bd3
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-08 11:52:33 +0530

    Time: 15 ms (54.59%), Space: 25.8 MB (27.2%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,37 @@
 #define ll long long
 class Solution {
 public:
+    int superSum = 0;
     ll solve(vector<int>& arr, int target, int idx, ll sum,
-             map<array<ll,2>,ll>& dp) {
+             map<array<ll, 2>, ll>& dp) {
         if (idx >= arr.size())
             return sum == target ? 1 : 0;
 
-        if (dp.count({idx,sum}))
-            return dp[{idx,sum}];
+        if (dp.count({idx, sum}))
+            return dp[{idx, sum}];
         ll subtract = solve(arr, target, idx + 1, sum - arr[idx], dp);
         ll add = solve(arr, target, idx + 1, sum + arr[idx], dp);
 
-        return dp[{idx,sum}] = subtract + add;
+        return dp[{idx, sum}] = subtract + add;
+    }
+    int solveUsingVectorDP(vector<int>& arr, int target, int idx, ll sum,
+                           vector<vector<int>>& dp) {
+        if (idx >= arr.size())
+            return sum == target ? 1 : 0;
+
+        if (dp[idx][superSum + sum]!=-1)
+            return dp[idx][superSum + sum];
+        ll subtract = solveUsingVectorDP(arr, target, idx + 1, sum - arr[idx], dp);
+        ll add = solveUsingVectorDP(arr, target, idx + 1, sum + arr[idx], dp);
+
+        return dp[idx][superSum + sum] = subtract + add;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        // vector<vector<int>>dp(n,vector<int>(2*sum + 1,-1));
-        map<array<ll,2>,ll> dp;
-        return solve(nums, target, 0, 0LL, dp);
+        superSum = accumulate(nums.begin(), nums.end(), 0);
+        vector<vector<int>>dp(n,vector<int>(2*superSum + 1,-1));
+        // map<array<ll, 2>, ll> dp;
+        // return solve(nums, target, 0, 0LL, dp);
+        return solveUsingVectorDP(nums,target,0,0,dp);
     }
 };
