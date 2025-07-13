b80a02fe37587021a22509f89f47eaa410f39847
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-13 11:51:31 +0530

    Time: 7 ms (80.95%), Space: 15.4 MB (33.78%) - LeetHub

--- 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.cpp
@@ -12,8 +12,9 @@ public:
         // base case
         if (curr >= cuboids.size())
             return 0;
-        int& ans = dp[curr][prev+1];
-        if(ans!=-1)return ans;
+        int& ans = dp[curr][prev + 1];
+        if (ans != -1)
+            return ans;
         int incl = 0;
         if (prev == -1 || isSafe(cuboids[prev], cuboids[curr])) {
             incl = cuboids[curr][2] + solveUsingRec(cuboids, curr + 1, curr);
@@ -23,17 +24,38 @@ public:
 
         return ans = max(incl, excl);
     }
+
+    int solveUsingTab(vector<vector<int>>& cuboids) {
+        int n = cuboids.size();
+        dp.resize(n + 1, vector<int>(n + 1, 0));
+
+        // curr ->[n-1 -> 0]
+        // prev -> [curr-1->-1]
+
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
+                int incl = 0;
+                if (prev == -1 || isSafe(cuboids[prev], cuboids[curr])) {
+                    incl = cuboids[curr][2] + dp[curr + 1][curr+1];
+                }
+
+                int excl = 0 + dp[curr + 1][prev+1];
+                dp[curr][prev+1] = max(incl, excl);
+            }
+        }
+        return dp[0][-1+1];
+    }
     int maxHeight(vector<vector<int>> cuboids) {
         int n = cuboids.size();
-        dp.resize(n+1,vector<int>(n+1,-1));
+        // dp.resize(n + 1, vector<int>(n + 1, -1));
         for (auto& cuboid : cuboids) {
             sort(cuboid.begin(), cuboid.end());
         }
         sort(cuboids.begin(), cuboids.end());
-        
 
         int curr = 0;
         int prev = -1;
-        return solveUsingRec(cuboids, curr, prev);
+        // return solveUsingRec(cuboids, curr, prev);
+        return solveUsingTab(cuboids);
     }
 };
