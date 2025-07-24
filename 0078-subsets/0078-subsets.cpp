323448468dbcaffbb358cd7eca3ecaa137195c68
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-24 09:36:01 +0530

    Time: 0 ms (100%), Space: 10 MB (60.7%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n=nums.size();
-        int upperBound = pow(2,n);
-        vector<vector<int>> res;
-        for(int i= 0;i<upperBound;i++){
-            int mask = i;
-            int pos = -1;
-            vector<int> ans;
-            while(mask!=0){
-                pos++;
-                if(mask&1){
-                    ans.push_back(nums[pos]);
+        int n= nums.size();
+        int mask = (1<<n);
+        vector<vector<int>> ans;
+        for(int i= 0;i<mask;i++){
+            vector<int>tempAns;
+            for(int j=0;j<n;j++){
+                if((i>>j)&1){
+                    tempAns.push_back(nums[j]);
                 }
-                mask>>=1;
             }
-            res.push_back(ans);
+
+            ans.push_back(tempAns);
         }
 
-        return res;
-
+        return ans;
     }
 };
