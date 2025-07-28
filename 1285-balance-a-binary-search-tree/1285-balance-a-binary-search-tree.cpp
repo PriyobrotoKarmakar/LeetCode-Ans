6b829a9530b01c75c93da0c936337e9c63a8fd40
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-07-28 09:57:03 +0530

    Time: 17 ms (44.41%), Space: 65.7 MB (46.07%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -1,80 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int height(TreeNode* root) {
-        if (!root) return 0;
-        return 1 + max(height(root->left), height(root->right));
+    vector<int> inorder;
+    void inordered(TreeNode* node){
+        if(!node)return;
+
+        inordered(node->left);
+        inorder.push_back(node->val);
+        inordered(node->right);
     }
+    TreeNode* construct(int left,int right){
+        if(left>right)return NULL;
+        int mid = (left+right)/2;
 
-    int getBalance(TreeNode* root) {
-        if (!root) return 0;
-        return height(root->left) - height(root->right);
+        TreeNode* node = new TreeNode(inorder[mid]);
+
+        node->left = construct(left,mid-1);
+        node->right = construct(mid+1,right);
+
+        return node;
     }
-
-    TreeNode* leftRotate(TreeNode* x) {
-        TreeNode* y = x->right;
-        TreeNode* T2 = y->left;
-
-        y->left = x;
-        x->right = T2;
-
-        return y;
-    }
-
-    TreeNode* rightRotate(TreeNode* y) {
-        TreeNode* x = y->left;
-        TreeNode* T2 = x->right;
-
-        x->right = y;
-        y->left = T2;
-
-        return x;
-    }
-
-    TreeNode* avlTreeInsertion(TreeNode* root, int val) {
-        if (!root) return new TreeNode(val);
-
-        if (val < root->val)
-            root->left = avlTreeInsertion(root->left, val);
-        else if (val > root->val)
-            root->right = avlTreeInsertion(root->right, val);
-        else
-            return root;
-
-        int balance = getBalance(root);
-
-
-        if (balance > 1 && val < root->left->val)
-            return rightRotate(root);
-
-     
-        if (balance < -1 && val > root->right->val)
-            return leftRotate(root);
-
-        
-        if (balance > 1 && val > root->left->val) {
-            root->left = leftRotate(root->left);
-            return rightRotate(root);
-        }
-
-      
-        if (balance < -1 && val < root->right->val) {
-            root->right = rightRotate(root->right);
-            return leftRotate(root);
-        }
-
-        return root;
-    }
-
-    void inorderInsert(TreeNode* node, TreeNode*& avlRoot) {
-        if (!node) return;
-        inorderInsert(node->left, avlRoot);
-        avlRoot = avlTreeInsertion(avlRoot, node->val);
-        inorderInsert(node->right, avlRoot);
-    }
-
     TreeNode* balanceBST(TreeNode* root) {
-        TreeNode* avlRoot = nullptr;
-        inorderInsert(root, avlRoot);
-        return avlRoot;
+        inordered(root);
+        return construct(0,inorder.size()-1);
     }
 };
