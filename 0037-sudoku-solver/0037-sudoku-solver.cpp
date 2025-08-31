29af67008e7c88efac59b2e919facac056b0fc5d
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-08-31 21:06:09 +0530

    Time: 358 ms (53.92%), Space: 8.8 MB (62.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,char digit,vector<vector<char>>& board){
-        for(int k=0;k<9;k++){
-            if(board[row][k]==digit){
+    bool isSafe(int row, int col, char digit, vector<vector<char>>& board) {
+        for (int k = 0; k < 9; k++) {
+            if (board[row][k] == digit) {
                 return false;
             }
         }
-        for(int k=0;k<9;k++){
-            if(board[k][col]==digit){
+        for (int k = 0; k < 9; k++) {
+            if (board[k][col] == digit) {
                 return false;
             }
         }
-        for(int k=0;k<9;k++){
-            if(board[3*(row/3)+k/3][3*(col/3)+k%3]==digit){
+        for (int k = 0; k < 9; k++) {
+            if (board[3 * (row / 3) + k / 3][3 * (col / 3) + k % 3] == digit) {
                 return false;
             }
         }
         return true;
-
     }
-    bool solve(vector<vector<char>>& board){
-        for(int row=0;row<board.size();row++){
-            for(int col=0;col<board[0].size();col++){
-                if(board[row][col]=='.'){
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < board.size(); row++) {
+            for (int col = 0; col < board[0].size(); col++) {
+                if (board[row][col] == '.') {
 
-
-                    for(char digit = '1';digit<='9';digit++){
-                        if(isSafe(row,col,digit,board)){
-                            board[row][col]=digit;
-                            bool furtherCheck=solve(board);
-                            if(furtherCheck){
+                    for (char digit = '1'; digit <= '9'; digit++) {
+                        if (isSafe(row, col, digit, board)) {
+                            board[row][col] = digit;
+                            bool furtherCheck = solve(board);
+                            if (furtherCheck) {
                                 return true;
-                            }
-                            else{
-                                board[row][col]='.';
+                            } else {
+                                board[row][col] = '.';
                             }
                         }
                     }
@@ -43,8 +40,5 @@ public:
         }
         return true;
     }
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-      
-    }
+    void solveSudoku(vector<vector<char>>& board) { solve(board); }
 };
