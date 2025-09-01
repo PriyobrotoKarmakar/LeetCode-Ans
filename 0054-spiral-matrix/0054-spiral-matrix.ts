89dc4ea24564339e3a98ec6fdaa2fdfc8eb7bcd9
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-01 13:50:07 +0530

    Time: 0 ms (100%), Space: 56 MB (5.47%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.ts
@@ -0,0 +1,35 @@
+function spiralOrder(matrix: number[][]): number[] {
+    let ans: number[] = [];
+    let id = 0;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let up = 0;
+    let down = matrix.length - 1;
+
+    while (up <= down && left <= right) {
+        if (id === 0) {
+            for (let i = left; i <= right; i++) {
+                ans.push(matrix[up][i]);
+            }
+            up++;
+        } else if (id === 1) {
+            for (let i = up; i <= down; i++) {
+                ans.push(matrix[i][right]);
+            }
+            right--;
+        } else if (id === 2) {
+            for (let i = right; i >= left; i--) {
+                ans.push(matrix[down][i]);
+            }
+            down--;
+        } else {
+            for (let i = down; i >= up; i--) {
+                ans.push(matrix[i][left]);
+            }
+            left++;
+        }
+        id = (id + 1) % 4;
+    }
+
+    return ans;
+}
