443302e62f9c26d8ce1f243c820e47257f8f8e1b
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-09 12:15:18 +0530

    Time: 2 ms (22.97%), Space: 8.3 MB (71.93%) - LeetHub

---
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -0,0 +1,40 @@
+#define p pair<int, char>
+class Solution {
+public:
+    string reorganizeString(string s) {
+        priority_queue<p, vector<p>> pq;
+        int limit = ceil(s.length() / 2.0);
+        vector<int> freq(26, 0);
+        for (auto ch : s) {
+            freq[ch - 'a']++;
+            if (freq[ch - 'a'] > limit)
+                return "";
+        }
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (freq[i - 'a'] > 0)
+                pq.push({freq[i - 'a'], i});
+        }
+        string ans = "";
+        while (pq.size() >= 2) {
+            auto p1 = pq.top();
+            pq.pop();
+            auto p2 = pq.top();
+            pq.pop();
+
+            ans += p1.second;
+            p1.first--;
+            ans += p2.second;
+            p2.first--;
+
+            if(p1.first>0)pq.push(p1);
+            if(p2.first>0)pq.push(p2);
+        }
+
+        if (!pq.empty()) {
+            ans += pq.top().second;
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
