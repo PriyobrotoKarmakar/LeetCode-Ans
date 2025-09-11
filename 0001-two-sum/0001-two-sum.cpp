bc6610fbfdf9ae24a17d40d7ba999e42b2b861b2
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-11 11:32:44 +0530

    Time: 191 ms (5.09%), Space: 14.8 MB (50.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,12 +2,40 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (nums[i] + nums[j] == target)
-                    return {i, j};
+        vector<pair<int,int>> num;
+        for(int i = 0;i<n;i++){
+            num.push_back({nums[i],i});
+        }
+
+        sort(num.begin(),num.end());
+        for(auto ele: num){
+            cout<<ele.first<<" "<<ele.second<<endl;
+        }
+
+        int i = 0;
+        int j = num.size()-1;
+        while(i<=j){
+            int val1 = num[i].first;
+            int val2 = num[j].first;
+            cout<<val1<<"->"<<val2<<endl;
+            if((val1+val2) == target){
+                
+                return {num[i].second,num[j].second};
+            }else if((val1+val2)>target){
+                j--;
+            }else{
+                i++;
             }
         }
+
+
+        // for (int i = 0; i < n; i++) {
+
+        //     for (int j = i+1; j < n; j++) {
+        //         if (nums[i] + nums[j] == target)
+        //             return {i, j};
+        //     }
+        // }
 
         return {};
     }
