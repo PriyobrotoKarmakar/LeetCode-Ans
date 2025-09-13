2274f2eb89e338b83786ec98352a0ce3856f38f1
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-13 13:57:38 +0530

    Time: 3 ms (23.38%), Space: 15.4 MB (97.9%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.cpp
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.cpp
@@ -1,33 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> sortEvenOdd(vector<int>& nums) {
+    // Bubble sort for even indices (ascending order)
+    void bubbleSortEven(vector<int>& nums) {
         int n = nums.size();
-        vector<int> evens, odds;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 ==  0)
-                evens.push_back(nums[i]);
-            else
-                odds.push_back(nums[i]);
-        }
-        sort(evens.begin(), evens.end());
-        sort(odds.rbegin(), odds.rend());
-
-        vector<int> res;
-        int evenIndex = 0, oddIndex = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 ==  0) {
-                res.push_back(evens[evenIndex]);
-                evenIndex++;
-            }
-
-            else {
-                res.push_back(odds[oddIndex]);
-                oddIndex++;
+        for (int i = 0; i < n; i += 2) {  // start from even indices
+            for (int j = 0; j + 2 < n; j += 2) {  // jump 2 steps
+                if (nums[j] > nums[j + 2]) {
+                    swap(nums[j], nums[j + 2]);
+                }
             }
         }
+    }
 
-        return res;
+    // Bubble sort for odd indices (descending order)
+    void bubbleSortOdd(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; i += 2) {  // start from odd indices
+            for (int j = 1; j + 2 < n; j += 2) {  // jump 2 steps
+                if (nums[j] < nums[j + 2]) { // descending
+                    swap(nums[j], nums[j + 2]);
+                }
+            }
+        }
+    }
+
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        // Apply bubble sort separately
+        bubbleSortEven(nums);
+        bubbleSortOdd(nums);
+
+        return nums;
     }
 };
