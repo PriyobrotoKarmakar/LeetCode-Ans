fbd25f0d4d4796209e9e004484d1a361e789518e
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-15 11:40:19 +0530

    Time: 6 ms (29.73%), Space: 15.5 MB (85.38%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
+    bool static Comparator(int& a, int& b) {
+        string s1 = to_string(a);
+        string s2 = to_string(b);
+        return s1 + s2 > s2 + s1;
+    };
     string largestNumber(vector<int>& nums) {
-        auto Comparator = [](int& a,int&b){
-            string s1 = to_string(a);
-            string s2 = to_string(b);
-            return s1+s2>s2+s1;
-        };
-        sort(nums.begin(),nums.end(),Comparator);
-        string ans="";
-        for(const int& N:nums){
-            ans+=to_string(N);
+
+        sort(nums.begin(), nums.end(), Comparator);
+        string ans = "";
+        for (const int& N : nums) {
+            ans += to_string(N);
         }
-        if(ans[0]=='0'){
+        if (ans[0] == '0') {
             return "0";
         }
         return ans;
