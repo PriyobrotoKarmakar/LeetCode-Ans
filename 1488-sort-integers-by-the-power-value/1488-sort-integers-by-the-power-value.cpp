71dc8fa7f5c205271692fe66689c5ebfb9878f50
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-16 13:41:57 +0530

    Time: 11 ms (86.23%), Space: 11.1 MB (80.49%) - LeetHub

---
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int PowerValue(int n) {
+        int count = 0;
+        while (n != 1) {
+            if (n % 2 == 0)
+                n = n / 2;
+            else
+                n = (3 * n) + 1;
+
+            count++;
+        }
+
+        return count;
+    }
+    int getKth(int lo, int hi, int k) {
+        vector<pair<int, int>> pq;
+        for (int i = lo; i <= hi; i++) {
+            int powervalue = PowerValue(i);
+            pq.push_back({powervalue, i});
+        }
+        sort(pq.begin(), pq.end());
+
+        return pq[(k-1)%pq.size()].second;
+    }
+};
