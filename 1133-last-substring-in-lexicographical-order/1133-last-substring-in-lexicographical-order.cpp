5fedcc67918c37e22286fbf6b2b7ee3048f94782
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-09-24 12:42:19 +0530

    Time: 10 ms (51.02%), Space: 23.2 MB (24.9%) - LeetHub

---
+++ 1133-last-substring-in-lexicographical-order/1133-last-substring-in-lexicographical-order.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int n = s.length(), i = 0, j = 1, k = 0;
+        while(j+k<n){
+            //check the validation
+            if(s[i+k]==s[j+k]) k++;
+            else if(s[i+k]<s[j+k]){
+                i = max(i+k+1,j);
+                j = i+1;
+                k = 0;
+            }else{
+                j+=k+1;
+                k = 0;
+            }
+
+        }
+
+        return s.substr(i);
+    }
+};
