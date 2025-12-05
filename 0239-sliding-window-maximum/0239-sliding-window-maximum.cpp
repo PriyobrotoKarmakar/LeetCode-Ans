3d75219d526dd27279ed4759b0405f75f20e01a8
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-12-05 22:26:00 +0530

    Time: 26 ms (48.34%), Space: 139.2 MB (37.12%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,21 +3,29 @@ public:
     multiset<int> ms;
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 
-        vector<int> res;
-        for (int i = 0; i < nums.size(); i++) {
-            ms.insert(nums[i]);
+        deque<int> dq; // Stores indices
+        vector<int> result;
 
-            // remove the element which is outof the window
-            if (i >= k) {
-                auto it = ms.find(nums[i - k]);
-                ms.erase(it);
+        for (int i = 0; i < nums.size(); i++) {
+            // 1. Remove index out of current window (from front)
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
             }
 
-            if (i >= k-1) {
-                res.push_back(*ms.rbegin());
+            // 2. Remove smaller elements from the back (Maintain Decreasing
+            // Order) If current num > back num, the back num is useless.
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+            // 3. Add current index
+            dq.push_back(i);
+
+            // 4. Record result (front is always the max)
+            if (i >= k - 1) {
+                result.push_back(nums[dq.front()]);
             }
         }
-
-        return res;
+        return result;
     }
 };
