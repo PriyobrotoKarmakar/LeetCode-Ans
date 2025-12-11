2da7ed6f2857c33b1247ed5cd64f76a7a412212e
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-12-11 12:24:09 +0530

    Time: 0 ms (100%), Space: 22.6 MB (77.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int solveUsingMap(vector<int>& nums) {
-        unordered_map<int, bool> visited;
-        int i = 0;
-        int size = nums.size();
-        while (i < size) {
-            if (visited[nums[i]]) {
-                nums.erase(nums.begin() + i);
-                size = nums.size();
-                continue;
-            }
-            visited[nums[i]] = true;
-            i++;
-        }
+    int solveUsingTwoPointer(vector<int>& arr) {
+        int n = arr.size();
+        if (n <= 1)
+            return n;
 
-        return nums.size();
+        // Start from the second element
+        int idx = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[idx++] = arr[i];
+            }
+        }
+        return idx;
     }
 
-    int LinearSearch(vector<int>& nums){
+    int LinearSearch(vector<int>& nums) {
         vector<int> arr;
         arr.push_back(nums[0]);
-        for(int i =1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]){
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
                 arr.push_back(nums[i]);
             }
         }
         nums = arr;
         return nums.size();
     }
-    int removeDuplicates(vector<int>& nums) {
-        return LinearSearch(nums);
-    }
+    int removeDuplicates(vector<int>& nums) { return solveUsingTwoPointer(nums); }
 };
