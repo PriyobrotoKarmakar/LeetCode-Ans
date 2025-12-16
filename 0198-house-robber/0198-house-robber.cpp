294214d3b33b82577d054ff287e275aff6b1e12f
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-12-17 00:10:32 +0530

    Time: 3 ms (5.96%), Space: 10.6 MB (44.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    // int solve(vector<int>& nums, int i, vector<int>& dp) {
-    //     if (i == 0)
-    //         return nums[i];
-    //     if (i < 0)
-    //         return 0;
-    //     if (dp[i] != -1)
-    //         return dp[i];
-    //     int inc = nums[i] + solve(nums, i - 2, dp);
-    //     int exc = 0 + solve(nums, i - 1, dp);
+    int solve(vector<int>& nums, vector<int>& dp, int i = 0) {
+        if (i >= nums.size())
+            return 0;
+        int& ans = dp[i];
+        if (ans != -1)
+            return ans;
+        int include = nums[i] + solve(nums, dp, i + 2);
+        int exclude = solve(nums, dp, i + 1);
 
-    //     return dp[i] = max(inc, exc);
-    // }
-    int solveUsingBottomUP(vector<int>& nums, int n) {
-        vector<int> dp(n + 1, -1);
-        if(n==1) return nums[0];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for (int i = 2; i < n; i++) {
-            int inc = nums[i] + dp[i - 2];
-            int exc = 0 + dp[i - 1];
-            dp[i] = max(inc, exc);
-        }
-        return dp[n-1];
+        return ans = max(include, exclude);
     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        // vector<int> dp(n + 1, -1);
-        // int i = 0;
-        // int money = 0;
-        // return solve(nums, n - 1, dp);
-        return solveUsingBottomUP(nums,n);
+        vector<int> dp(nums.size() + 1, -1);
+        return solve(nums, dp);
     }
 };
