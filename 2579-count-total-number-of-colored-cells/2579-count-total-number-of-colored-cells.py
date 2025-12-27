82454ab2193da039d189ea764f6236705022e73b
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2025-12-28 00:59:23 +0530

    Time: 0 ms (100%), Space: 17.1 MB (100%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -1,11 +1,4 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        ans = 1
-        ans = ans + (4 * (n * (n - 1) // 2))  # Use // for integer division
-        return int(ans)
-
-        # n==1 => 1(prev)
-        # n==2 prev + 4*(n-1) = prev + 4 * 1 =>prev + 4 => 1 + 4 = 5(prev)
-        # n==3 prev + 4 * 2 = prev + 8 = > 13
-        # n==4  13 + 4 * 3 = 13 + 12 = 25.....
-        # n==5 prev + 4 * 4 =
+        no_of_boxes = n**2 + (n-1)**2
+        return no_of_boxes
