ef13790b164fd3d304aedca3fe72276a8412f59e
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-01-23 11:57:49 +0530

    Time: 196 ms (15.47%), Space: 57.9 MB (6.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -19,19 +19,49 @@ public:
 
         return dp[i] = false;
     }
-    bool solveUsingGreedy(vector<int>& nums){
+    bool solveUsingGreedy(vector<int>& nums) {
         int maxJump = 0;
-        for(int i = 0;i<nums.size();i++){
-            if(i>maxJump)return false;
-            maxJump = max(maxJump,i+nums[i]);
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxJump)
+                return false;
+            maxJump = max(maxJump, i + nums[i]);
         }
         return true;
+    }
+    bool canJumpUsingBFS(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1)
+            return true;
+        queue<int> q;
+        q.push(0);
+
+        vector<bool> visited(n, false);
+        visited[0] = true;
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+
+            for (int k = 1; k <= nums[curr]; ++k) {
+                int nextIndex = curr + k;
+
+                if (nextIndex >= n - 1)
+                    return true;
+
+                if (!visited[nextIndex]) {
+                    visited[nextIndex] = true;
+                    q.push(nextIndex);
+                }
+            }
+        }
+        return false;
     }
     bool canJump(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n + 1, -1);
         // bool ans = solve(nums, 0, dp);
-        bool ans = solveUsingGreedy(nums);
+        // bool ans = solveUsingGreedy(nums);
+        bool ans = canJumpUsingBFS(nums);
         return ans;
     }
 };
