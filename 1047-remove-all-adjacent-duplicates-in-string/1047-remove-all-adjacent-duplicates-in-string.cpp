3156305138bf31db16357f244be29bfe8b8d8047
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-01-25 23:39:19 +0530

    Time: 8 ms (38.23%), Space: 14.4 MB (29.9%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-       string ans = "";
-       int index = 0;
-       while(index<s.length()){
-           if(ans.length() > 0 && ans[ans.length()-1]==s[index]){
-               ans.pop_back();
-           }
-           else{
-               ans.push_back(s[index]);
-           }
-           index++;
-       } 
-       return ans;
+        stack<char> st;
+        for(char& ch: s){
+            if(!st.empty() && st.top()==ch){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
