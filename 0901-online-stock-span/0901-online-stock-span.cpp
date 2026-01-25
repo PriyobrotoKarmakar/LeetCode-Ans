3f99152ca84e0ae83f1c4c768b0d73c6b698a660
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-01-26 00:21:51 +0530

    Time: 28 ms (68.89%), Space: 92.1 MB (58.86%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,21 +1,21 @@
 class StockSpanner {
+private:
+    stack<pair<int,int>> st;
+    
 public:
-    stack<pair<int, int>> st;
     StockSpanner() {
-        
+
     }
     
     int next(int price) {
-        int span = 1; // The day itself counts as 1
-        
-        // Monotonic check: merge spans of smaller previous prices
-        while (!st.empty() && st.top().first <= price) {
-            span += st.top().second; // Add the previous span to current
+        int span = 1;
+        while(!st.empty() && st.top().first<=price){
+            span+=st.top().second;
             st.pop();
         }
-        
-        // Push the calculated span for this price
-        st.push({price, span});
+
+
+        st.push({price,span});
         
         return span;
     }
