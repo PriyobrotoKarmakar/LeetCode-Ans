9c288fc7204a8022ce20163b4090640b5941affa
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-01-25 19:41:21 +0530

    Time: 7 ms (65.56%), Space: 26.2 MB (47.57%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int div, int thres) {
+    bool isValid(vector<int>&nums,int thresshold,int mid){
         int sum = 0;
-        for (auto ele : nums) {
-            sum += ceil((double)ele / (double)div);
-        }
-        return sum <= thres;
+        for(auto& ele: nums){
+            sum+=ceil((double)ele/(double)mid);
+        } 
+        return sum<=thresshold;
     }
     int smallestDivisor(vector<int>& nums, int threshold) {
         int start = 1;
-        int end = *max_element(nums.begin(), nums.end());
+        int end = *max_element(nums.begin(),nums.end());
         int ans = 0;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (solve(nums, mid, threshold)) {
-                end = mid - 1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(isValid(nums,threshold,mid)){
                 ans = mid;
-            } else {
-                start = mid + 1;
+                cout<<mid<<"->";
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
         return ans;
