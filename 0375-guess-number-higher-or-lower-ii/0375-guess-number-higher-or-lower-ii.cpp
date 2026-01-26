49abd61d6f355c441d8dc602186f814002653077
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-01-26 16:57:16 +0530

    Time: 39 ms (68.36%), Space: 11.2 MB (8.23%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int solve(int start, int end) {
+    int solveUsingRec(int start, int end) {
         // base case
         if (start >= end)
             return 0;
-        
+
         int& dpAns = dp[start][end];
-        if(dpAns!=-1)return dpAns;
+        if (dpAns != -1)
+            return dpAns;
         int ans = INT_MAX;
         for (int x = start; x <= end; x++) {
-            int left = solve(start, x - 1);
-            int right = solve(x + 1, end);
+            int left = solveUsingRec(start, x - 1);
+            int right = solveUsingRec(x + 1, end);
             ans = min(ans, x + max(right, left));
         }
         return dpAns = ans;
     }
+    int solveUsingTab(int n) {
+        dp.resize(n + 10, vector<int>(n + 10, 0));
+
+        for (int l = n; l >= 1; l--) {
+            for (int r = 1; r <= n; r++) {
+                if (l >= r)
+                    continue;
+
+                dp[l][r] = INT_MAX;
+
+                for (int x = l; x <= r; x++) {
+                    int cost = x + max(dp[l][x - 1], dp[x + 1][r]);
+                    dp[l][r] = min(dp[l][r], cost);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
 
         int ans = 0;
-        dp.resize(n+1,vector<int>(n+1,-1));
-        ans = solve(1, n);
+        // dp.resize(n+1,vector<int>(n+1,-1));
+        // ans = solveUsingMemo(1, n);
+        ans = solveUsingTab(n);
         return ans;
     }
 };
