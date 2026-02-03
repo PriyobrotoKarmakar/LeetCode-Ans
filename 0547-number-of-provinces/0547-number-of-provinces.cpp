3a0a23f8e42e50d292a75c9814e098b527cdb6d7
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-02-03 12:28:58 +0530

    Time: 4 ms (27.35%), Space: 19.5 MB (73.99%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,53 +1,40 @@
-#include <vector>
-#include <numeric>
-
-using namespace std;
-
-class DSU {
-public:
+class DSU{
+    public:
     vector<int> leader;
-    
-    DSU(int n) {
+    DSU(int n){
         leader.resize(n);
-        iota(leader.begin(), leader.end(), 0);
+        iota(leader.begin(),leader.end(),0);
     }
-    
-    int find(int i) {
-        if(leader[i] == i) return i;
-        return leader[i] = find(leader[i]); 
+    int find(int i){
+        if(leader[i]==i) return i;
+        return find(leader[i]);
     }
-
-    void unite(int i, int j) {
+    void unite(int i,int j){
         int leader1 = find(i);
         int leader2 = find(j);
-
-        if(leader1 != leader2) {
-            leader[leader1] = leader2;
-        }
+        if(leader1==leader2) return;
+        leader[leader1] = leader2;
     }
 };
-
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        DSU dsu(n); 
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1) {
-                    dsu.unite(i, j);
-                }
+        int m = isConnected[0].size();
+        DSU dsu(n);
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(isConnected[i][j]==1)
+                    dsu.unite(i,j);
+            }
+        }
+        for(int i = 0;i<n;i++){
+            if(dsu.leader[i]==i){
+                ans++;
             }
         }
 
-        int provinces = 0;
-        for(int i = 0; i < n; i++) {
-            if(dsu.leader[i] == i) {
-                provinces++;
-            }
-        }
-        
-        return provinces;
+        return ans;
     }
 };
