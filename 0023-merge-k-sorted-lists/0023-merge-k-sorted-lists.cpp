a95e87a6d8be9f1dc544df69425a5e3fc1ea5b56
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-03-02 20:05:23 +0530

    Time: 0 ms (100%), Space: 18.3 MB (81.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,43 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class comparetor {
-public:
-    bool operator()(ListNode*& a, ListNode*& b) { return a->val > b->val; }
+class comperator{
+    public:
+        bool operator()(ListNode* &a,ListNode* &b){
+            return a->val > b->val;
+        }
 };
 class Solution {
+    
+    void insertAtTail(ListNode* &head,ListNode* &tail,ListNode* &nextNode){
+        if(head==NULL and tail==NULL){
+            head = nextNode;
+            tail = nextNode;
+        }else{
+            tail->next = nextNode;
+            tail = tail->next;
+        }
+    }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, comparetor> pq;
-        int row = lists.size();
-        for (int i = 0; i < row; i++) {
-            ListNode* temp = lists[i];
-            if (temp != NULL) {
-                pq.push(temp);
-            }
+        priority_queue<ListNode*,vector<ListNode*>,comperator> pq;
+
+        for(ListNode* head: lists){
+            if(head!=NULL) pq.push(head);
         }
 
         ListNode* head = NULL;
         ListNode* tail = NULL;
+        while(!pq.empty()){
+            ListNode* nextNode = pq.top(); pq.pop();
 
-        while (!pq.empty()) {
-            ListNode* front = pq.top();
-            pq.pop();
-
-            // insertingAtTail
-            if (head == NULL and tail == NULL) {
-                head = front;
-                tail = front;
-            } else {
-                tail->next = front;
-                tail = front;
-            }
-
-            if (front->next != NULL) {
-                pq.push(front->next);
-            }
+            insertAtTail(head,tail,nextNode);
+            if(nextNode->next!=NULL) pq.push(nextNode->next);
         }
-
         return head;
     }
 };
