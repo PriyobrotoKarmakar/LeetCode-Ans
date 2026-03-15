70a6c938d22af468be6a1834c00956d7e8d8144c
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-03-15 20:23:46 +0530

    Time: 191 ms (54.07%), Space: 19.4 MB (90.66%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.py
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.py
@@ -3,27 +3,17 @@ class Solution:
     def subarrayLCM(self, nums: List[int], k: int) -> int:
         #lcm(1,5) = 5
         #1 2 3 4 5
+        def lcm(firstNumber,secondNumber):
+            return (firstNumber*secondNumber)//math.gcd(firstNumber,secondNumber)
         n = len(nums)
         ans = 0
-        for i in range(n):
+        for i in range(n): #O(n)
             currLcm = nums[i]
-            for j in range(i,n):
-                currLcm = math.lcm(currLcm,nums[j])
+            for j in range(i,n): #O(n)
+                currLcm = lcm(currLcm,nums[j])
                 if(currLcm==k):
                     ans+=1
+                elif currLcm > k:
+                    break
         return ans
         
-# import math
-# class Solution:
-#     def subarrayLCM(self, nums: List[int], k: int) -> int:
-#         count = 0
-#         n = len(nums)
-#         for i in range(n):
-#             for j in range(i, n):
-#                 a = nums[i:j+1]
-#                 b = math.lcm(*a)
-#                 if b == k:
-#                     count += 1
-#                 if b > k:
-#                     break
-#         return count
