ab0a8f04638466b9796f5151dfa1caa25c88caa8
Priyobroto Karmakar <126158991+PriyobrotoKarmakar@users.noreply.github.com>
2026-06-13 20:58:39 +0530

    Time: 4 ms (3.39%), Space: 17.2 MB (14.57%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        for(auto num: nums){
+            ans.push_back(num);
+        }
+        for(auto num: nums){
+            ans.push_back(num);
+        }
+
+        return ans;
+    }
+};
